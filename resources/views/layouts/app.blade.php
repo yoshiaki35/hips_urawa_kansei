0cda666980e66d7398e0704356b814a377c5e7d8
EC2 Default User <ec2-user@ip-10-0-0-159.ap-northeast-1.compute.internal>
2020-08-31 12:09:01 +0900

    追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,129 @@
 </head>
 <body>
     @yield('content')
-        
- 
+    <div class="contact sp-none">
+        <h2>
+            ご不明点等がございましたらお気軽にお問い合わせください。
+            <br>
+            ホームページからの御予約もかのです
+        </h2>
+        <ul>
+            <li>
+                <div class="detail">
+                    <p class="title">ご予約フォーム</p>
+                    メール御予約は当店１回以上ご利用された会員様に限ります。
+                    <br>
+                    御予約日の一週間前の深夜０時から受付開始となります。
+                    <div class="loading-panel-wrap">
+                        <a href="reserve">
+                            <div class="loading-panel">
+                                <span>
+                                    <h3>
+                                        <i class="fa fa-file-text-o"></i>
+                                        ご予約はコチラ
+                                    </h3>
+                                </span>
+                            </div>
+                        </a>
+                        <div class="shadow"></div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="detail">
+                    <p class="title">お電話でのお問い合わせ</p>
+                    お急ぎやの方や詳しい話を聞きたい方はお電話ください
+                    <br>
+                    非通知設定の場合は解除お願いします。
+                    <div class="loading-panel-wrap">
+                        <div class="loading-panel">
+                            <span>
+                                <h3 x-ms-format-detection="none"></h3>
+                                <h4>10:00～23:00(電話受付9:30～21:00)</h4>
+                            </span>
+                        </div>
+                        <div class="shadow"></div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="sp-f-contents pc-none">
+        <h2 class="none">
+            Contents
+            <span>メニュー項目</span>
+        </h2>
+        <ul>
+            <li>
+                <a href="information">
+                    <img data-src="img/f-nav-news.png" alt="最新情報"　class="lazyload" src="img/dummy.png">
+                </a>
+            </li>
+            <li class="sub">
+                <a href="{{ url('cast') }}">
+                    <img data-src="img/f-nav-girls.png" alt="在籍女性" class="lazyload" src="img/dummy.png">
+                </a>
+            </li>
+            <li>
+                <a href="schedule">
+                    <img data-src="img/f-nav-schedule.png" alt="出勤情報" class="lazyload" src="img/dummy.png">
+                </a>
+            </li>
+        </ul>
+
+    </div>
+    <div class="shoplist sp-none">
+    <div class="shoplist sp-none">
+        <div class="saitama">
+            <h3 class="area">
+                <i class="fa fa-building"></i>
+                埼玉エリア
+            </h3>
+            <p>
+                <i class="fa fa-dot-cirecle-o"></i>
+                <a href="https://hips.jp" target="_blank">越谷本店</a>
+            </p>
+            <p>
+                <i class="fa fa-dot-cirecle-o"></i>
+                <a href="https://hips-nishikawa.jp" target="_blank">西川口店</a>
+            </p>
+            <p>
+                <i class="fa fa-dot-cirecle-o"></i>
+                <a href="https://hipskumagaya.com" target="_blank">熊谷店</a>
+            </p>
+            <p>
+                <i class="fa fa-dot-cirecle-o"></i>
+                <a href="https://hipssm.blog.fc2.com" target="_blank">高級派遣SM倶楽部・越谷店</a>
+            </p>
+            <p>
+                <i class="fa fa-dot-cirecle-o"></i>
+                <a href="https://hipssm.blog.fc2.com" target="_blank">高級派遣SM倶楽部・西川口店</a>
+            </p>
+            <p>
+                <i class="fa fa-dot-cirecle-o"></i>
+                <a href="https://hips.jp" target="_blank">越谷本店</a>
+            </p>
+            <p>
+                <i class="fa fa-dot-cirecle-o"></i>
+                <a href="https://hips.jp" target="_blank">越谷本店</a>
+            </p>
+            <p>
+                <i class="fa fa-dot-cirecle-o"></i>
+                <a href="https://hips.jp" target="_blank">越谷本店</a>
+            </p>
+            <p>
+                <i class="fa fa-dot-cirecle-o"></i>
+                <a href="https://hips.jp" target="_blank">越谷本店</a>
+            </p>
+            <p>
+                <i class="fa fa-dot-cirecle-o"></i>
+                <a href="https://hips.jp" target="_blank">越谷本店</a>
+            </p>
+            <p>
+                <i class="fa fa-dot-cirecle-o"></i>
+                <a href="https://hips.jp" target="_blank">越谷本店</a>
+            </p>
+        </div>
+    </div>
 </body>
 </html>
